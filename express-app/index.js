065284c5242fe950fc4b3b3b615c44a117e26ffc
gitloveJefdog <soccer_is_loved_by_me_0507@ezweb.ne.jp>
2018-09-23 11:41:57 +0000

    form

--- express-app/index.js
+++ express-app/index.js
@@ -8,21 +8,41 @@ app.engine('ejs',ejs.renderFile);
 
 app.use(express.static('public'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
 //indexページの作成
 app.get('/',(req,res)=>{
     //index.ejsをレンダリングする
+    var msg = 'This is Index page! これはトップページです．メッセージを書いて送信してください';
+    var url = '/other?name=taro&pass=yamada';
     res.render('index.ejs',
                 {title:'Index',
-                content:'This is Express-app Top page with style.css !',
-                link:{href:'/other',text:'*別のページに移動'}
+                content:msg,
+                link:{href:url,text:'*別のページに移動'}
             });
+});
+
+//post送信の処理
+app.post('/',(req,res)=>{
+    var msg = 'This is posted Page!あなたは「'+req.body.message+'」と送信しました．';
+    var url = '/other?name=taro&pass=yamada';
+    res.render('index.ejs',
+    {
+      title:'Posted',
+      content:msg,  
+      link:{href:url,text:'*別のページに移動'},
+    });
 });
 
 //otherページの作成
 app.get('/other',(req,res)=>{
+    var name = req.query.name;
+    var pass = req.query.pass;
+    var msg = 'あなたの名前は「'+name+' 」パスワードは「'+pass+'」です．';
     res.render('index.ejs',{
         title:'other',
-        content:'This is other page with style.css',
+        content:msg,
         link:{href:'/',text:'*トップページに移動'}
     })
 });
