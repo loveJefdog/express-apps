e46c64d22f3bbf0db40acd29672df7b8bb856a93
gitloveJefdog <soccer_is_loved_by_me_0507@ezweb.ne.jp>
2018-10-03 11:05:33 +0000

    rssの利用

--- express-app/ex-gen-app/routes/hello.js
+++ express-app/ex-gen-app/routes/hello.js
@@ -2,31 +2,30 @@
 var express = require('express');
 var router = express.Router();
 
+var http = require('https');
+var parseString = require('xml2js').parseString;
+
 router.get('/',(req,res,next)=>{
-    var msg = '＊何か書いてください'
-   // var name = req.query.name;
-   // var mail = req.query.mail;
-
-   if(req.session.hoge != undefined){
-       msg ='Last message:'+req.session.hoge;
-   }
-
-    var data = {
-        title:'Hello!',
-        content:msg,
+    var opt = {
+        host:'news.google.com',
+        port:443,
+        path:'/news?hl=ja&ned=us&ie=UTF-8&output=rss'
     };
-    res.render('hello',data);
-});
-
-router.post('/post',(req,res,next)=>{
-    var msg = req.body['message'];
-    req.session.hoge = msg;
-    var data = {
-        title:'Hello!',
-        content:'Last message:'+req.session.hoge,
-    };
-    res.render('hello',data);
-
+    http.get(opt,(res2)=>{
+        var body = " ";
+        res2.on('data',(data)=>{
+            body +=data;
+        });
+        res2.on('end',()=>{
+            parseString(body.trim(),(err,result)=>{
+                var data ={
+                    title:'Hello!',
+                    content: result.rss.channel[0].item
+                };
+                res.render('hello',data);
+            });
+        });
+    });
 });
 
 module.exports = router;
