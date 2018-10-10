a6d2d387ebccb6e8f1a7ba0947a31357ccda8866
gitloveJefdog <soccer_is_loved_by_me_0507@ezweb.ne.jp>
2018-10-10 05:49:22 +0000

    db create read

--- express-app/ex-gen-app/routes/hello.js
+++ express-app/ex-gen-app/routes/hello.js
@@ -2,30 +2,56 @@
 var express = require('express');
 var router = express.Router();
 
-var http = require('https');
-var parseString = require('xml2js').parseString;
+var sqlite3 = require('sqlite3');
+
+var db = new sqlite3.Database('mydb.sqlite3');
 
 router.get('/',(req,res,next)=>{
-    var opt = {
-        host:'news.google.com',
-        port:443,
-        path:'/news?hl=ja&ned=us&ie=UTF-8&output=rss'
-    };
-    http.get(opt,(res2)=>{
-        var body = " ";
-        res2.on('data',(data)=>{
-            body +=data;
-        });
-        res2.on('end',()=>{
-            parseString(body.trim(),(err,result)=>{
-                var data ={
+    db.serialize(()=>{
+        db.all("select * from mydata;",(err,rows)=>{
+            console.log(rows);
+            if(!err){
+                var data = {
                     title:'Hello!',
-                    content: result.rss.channel[0].item
+                    content: rows,
                 };
-                res.render('hello',data);
-            });
+                res.render('hello/index',data);
+            }
         });
     });
+});
+
+router.get('/add',(req,res,next)=>{
+    var data = {
+        title:'Hello/Add',
+        content:'新しいレコードを入力:'
+    }
+    res.render('hello/add',data)
+});
+
+router.post('/add',(req,res,next)=>{
+    var nm = req.body.name;
+    var ml = req.body.mail;
+    var ag = req.body.age;
+    db.run('insert into mydata (name,mail,age) values (?,?,?)',nm,ml,ag);
+    res.redirect('/hello');
+});
+
+router.get('/show',(req,res,next)=>{
+    var id = req.query.id;
+    db.serialize(()=>{
+        var q = 'select * from mydata where id = ?';
+        db.get(q,[id],(err,row)=>{
+            if(!err){
+                var data = {
+                    title:'Hello/Show',
+                    content:'id ='+id+'のレコード',
+                    mydata: row
+                }
+                res.render('hello/show',data);
+            }
+        });
+    })
 });
 
 module.exports = router;
